dfbb03e2fb70e6e2c808b6037629ea9d7be6190d
msywill <77587905+msywill@users.noreply.github.com>
2023-02-28 14:37:22 +0800

    Delete data directory

--- data/argtest.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='test')
-    parser.add_argument('--test', default="input", type=str, help='input files')
-    parser.add_argument('--output_path', default="output", type=str, help='result dir')
-    args = parser.parse_args()
-    print(args.test)
-    print(args.output_path)
